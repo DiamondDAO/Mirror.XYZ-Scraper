7c588411b3631c1a96fc7513d888cfd896cd5d85
christian <christianlemp@gmail.com>
2022-02-25 16:16:23 -0700

    added parsing function

--- scripts/gatherTwitterAccounts.js
+++ scripts/gatherTwitterAccounts.js
@@ -9,17 +9,41 @@ fs.readFile(filePath, function(err, data) {
     if (err) {
         console.log(err);
     } else {
-        let post = JSON.parse(JSON.parse(data));
-        let contributor = post["authorship"]["contributor"];
-        let title = post["content"]["title"];
-        let date_published = post["content"]["timestamp"];
-        let body = post["content"]["body"];
+        // let post = JSON.parse(JSON.parse(data));
+        // let contributor = post["authorship"]["contributor"];
+        // let title = post["content"]["title"];
+        // let date_published = post["content"]["timestamp"];
+        // let body = post["content"]["body"];
 
-        getTwitterAccounts(body);
+        // let associated_twitters = getTwitterAccounts(body);
+        let parsed_data = parseMirrorData(data);
         console.log(post["content"]);
         // console.log(JSON.parse(data));
     }
 });
+
+function parseMirrorData(content) {
+    dict = {};
+
+    // parse the Mirror post
+    let post = JSON.parse(JSON.parse(content));
+    let contributor = post["authorship"]["contributor"];
+    let title = post["content"]["title"];
+    let date_published = post["content"]["timestamp"];
+    let body = post["content"]["body"];
+    let digest = post["originalDigest"];
+    let associated_twitters = getTwitterAccounts(body);
+
+    // fill dictionary with parsed data
+    dict["contributor"] = contributor;
+    dict["title"] = title;
+    dict["date_published"] = date_published;
+    dict["body"] = body;
+    dict["originalDigest"] = digest;
+    dict["associated_twitters"] = associated_twitters;
+
+    return dict;
+}
 
 function getTwitterAccounts(content){
     twitter_account_list = [];
